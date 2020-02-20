0f39a3255c279439d3aaed108fd415d3c374dcb4
Th5tch3r <amber.phan27@gmail.com>
2020-02-19 23:28:12 -0500

    having password hash, token working, now just need to add restricted middleware for private route

--- api/server.js
+++ api/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const helmet = require('helmet');
-const jwt = require('jsonwebtoken');
+
+const authRouter = require('../auth/auth-router.js');
+const usersRouter = require('../users/users-router');
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+
+server.use('/api', authRouter);
+server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json({message: "This user api is ALIVE!"})
---
+++ auth/auth-router.js
@@ -0,0 +1,63 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js');
+
+const Users = require('../users/users-model.js');
+
+router.post('/register', (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({message: "Error adding this user"});
+        })
+})
+
+router.post('/login', (req, res) => {
+    let {username, password } = req.body;
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = genToken(user);
+
+                res.status(200).json({
+                    message: `Welcome ${user.username} to Thatcher's API!!`,
+                    token: token
+                });
+            } else {
+                res.status(401).json({message: "Invalid credentials"})
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({message: "Cannot log this user in"})
+        })
+})
+
+function genToken(user) {
+    const payload = {
+      userid: user.id,
+      username: user.username,
+      role: user.department
+      //other things: right? 
+    };
+  
+    const options = {
+      expiresIn: '1h'
+    };
+
+    const token = jwt.sign(payload, secrets.jwtSecret, options);
+  
+    return token;
+  }
+
+module.exports = router;
